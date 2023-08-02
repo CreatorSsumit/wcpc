e1c3ba397d707cd51b9bacea21d7cbdb37b8e513
Sumit Kumar Kushwaha <SKKushwaha@bpl.netlink.com>
2023-08-03 03:14:18 +0530

    add

--- src/chatBot/regexMatch.js
+++ src/chatBot/regexMatch.js
@@ -2,6 +2,6 @@ export const regexMatch = {
   adminclos: /administrative|closing/i,
   bonds: /bond/i,
   copyrequest: /copy|copy request/i,
-  wheredoifile: /courtroom|contact|courtroom$/i,
+  wheredoifile: /courtroom|contact|courtroom/i,
   ePaymentInstructions: /epayment|pay|online|e-payment/i,
 };
