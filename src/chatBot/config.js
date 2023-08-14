ddcc60c11889831e745ca1be1ab5e359f4013c7f
Sumit Kumar Kushwaha <SKKushwaha@bpl.netlink.com>
2023-08-14 17:22:45 +0530

    icons added

--- src/chatBot/config.js
+++ src/chatBot/config.js
@@ -1,11 +1,16 @@
 // in the config
 import React from "react";
 import { createChatBotMessage, createCustomMessage } from "react-chatbot-kit";
+import { AiOutlineMinusCircle } from "react-icons/ai";
 import QuestionList from "./QuestionList";
 import { faqQuestion } from "./faqQuestion";
 import AnswerList from "./AnswerList";
 
 const botName = "Wayne";
+
+const hideChatBox = () => {
+  window.parent.postMessage("hide_chatBot", "*");
+};
 
 const config = {
   botName: botName,
@@ -33,6 +38,17 @@ const config = {
   initialMessages: [
     createChatBotMessage(`"Hi, I'm ${botName}. How can I assist you today?"`),
   ],
+  customComponents: {
+    header: () => (
+      <div className="chatHeader">
+        Conversation with {botName}
+        <AiOutlineMinusCircle
+          className="chatBotHeaderIcon"
+          onClick={hideChatBox}
+        />
+      </div>
+    ),
+  },
 };
 
 export default config;
